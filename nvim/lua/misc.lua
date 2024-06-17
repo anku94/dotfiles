c96807954b289fdda1481c4dbcab4db636d2296f
Ankush Jain <anku.94@gmail.com>
2024-06-17 15:00:06 -0400

    nvim fix copilot tex disable

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -97,6 +97,5 @@ vim.g.vimtex_view_method = 'skim'
 vim.g.vimtex_view_skim_sync = 1
 vim.g.vimtex_view_skim_activate = 1
 
-vim.g.copilot_filetypes = {
-  'tex': false
-}
+vim.g.copilot_filetypes = {tex = false}
+
