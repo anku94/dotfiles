25b5684d91132eba9dd07d351f97419d3394c207
Ankush Jain <anku.94@gmail.com>
2024-10-30 12:54:09 -0400

    autoreload ipython

--- ipython/00-autoreload.py
+++ ipython/00-autoreload.py
@@ -2,3 +2,4 @@
 
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
+# get_ipython().run_line_magic('matplotlib', 'osx') # - uncomment for MacOS
